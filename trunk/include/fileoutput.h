0294bb1c19ff4554fb8e97cb063d90382553f17f
redcomet87 <redcomet87@3e54fc21-7451-0410-8dcd-cfc14eb3ad3a>
2008-07-14 22:05:41 +0000

    added file output

---
+++ trunk/include/fileoutput.h
@@ -0,0 +1,47 @@
+/*
+Copyright (C) 2008 by Charles Solar
+charlessolar@gmail.com
+
+This file is part of bafprp.
+
+bafprp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+bafprp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with bafprp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BAFPRPFILE_H
+#define BAFPRPFILE_H
+
+#include "output.h"
+
+namespace bafprp
+{
+	class File : public Output
+	{
+	public:
+		// Register the output type
+		File();
+		~File();
+
+		void record( BafRecord* record );
+		void error( BafRecord* record, const std::string error );
+		void log( const std::string log );
+
+	private:
+		FILE* _fp;
+		// This variable simply initializes a class that registers with the main output code
+		static File registerThis;
+	};
+}
+
+
+#endif
---
+++ trunk/src/fileoutput.cpp
@@ -0,0 +1,77 @@
+/*
+Copyright (C) 2008 by Charles Solar
+charlessolar@gmail.com
+
+This file is part of bafprp.
+
+bafprp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+bafprp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with bafprp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "fileoutput.h"
+
+#include <stdio.h>
+
+namespace bafprp
+{
+	File File::registerThis;
+
+	File::File() : Output( "file" )
+	{
+		// Not sure if its a good idea to lock a file for the duration of the program,
+		// but I cannot find a way to unlock the file.
+		if( fopen_s( &_fp, "bafprp.log", "a+" ) != 0 )
+		{
+			Output::setOutput( "console" );
+			LOG_ERROR( "Could not open log file, falling back to console log" );
+		}
+	}
+
+	File::~File()
+	{
+		if( _fp ) fclose( _fp );
+	}
+
+	void File::error( BafRecord* record, const std::string error )
+	{
+		if( !_fp )
+		{
+			Output::setOutput( "console" );
+			LOG_ERROR( "File pointer no longer valid, falling back to console output" );
+		}
+
+		LOG_TRACE( "File::error" );
+		LOG_TRACE( "/File::error" );
+	}
+
+	void File::log( const std::string log )
+	{
+		if( !_fp )
+		{
+			Output::setOutput( "console" );
+			LOG_ERROR( "File pointer no longer valid, falling back to console output" );
+		}
+		fwrite( log.c_str(), 1, log.length(), _fp );
+	}
+
+	void File::record( BafRecord* record )
+	{
+		if( !_fp )
+		{
+			Output::setOutput( "console" );
+			LOG_ERROR( "File pointer no longer valid, falling back to console output" );
+		}
+		LOG_TRACE( "File::record" );
+		LOG_TRACE( "/File::record" );
+	}
+}
