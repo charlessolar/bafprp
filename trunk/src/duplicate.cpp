1ab066962142aa839659d707bd849d25eb186acf
redcomet87 <redcomet87@3e54fc21-7451-0410-8dcd-cfc14eb3ad3a>
2008-07-23 15:48:15 +0000

    duplicate processing is working

--- trunk/src/duplicate.cpp
+++ trunk/src/duplicate.cpp
@@ -30,27 +30,38 @@ namespace bafprp
 
 	void Duplicate::remove( std::vector<IBafRecord*>& records )
 	{
-		// algorithm.h <3
 		std::sort( records.begin(), records.end(), recordsort );
-		records.erase( std::unique( records.begin(), records.end(), recordequal ), records.end() );
+
+		// Notes:
+		// This statement did not work for me.  I do not know if it was working but giving the wrong output,
+		// or if it just was not working.  
+		// What I did was unique the list and tried to find the collisions in the vector from the values after
+		// the iterator returned from unique, which should be the start of the duplicates.
+		// I ended up getting very poor results from this search, often finding that records after
+		// the unique iterator were actually unique in the record log.  This caused me to remove this unique
+		// and add my own since once the records are sorted its fairly easy (and fast) to do it myself.
+
+		//records.erase( std::unique( records.begin(), records.end(), recordequal ), records.end() );
+
+		for(std::vector<IBafRecord*>::iterator itr = records.begin() + 1; itr != records.end(); itr++ )
+		{
+			if( (*itr)->getCRC() == (*(itr - 1))->getCRC() )
+			{
+				itr = records.erase( itr );
+			}
+		}
 	}
 
 	void Duplicate::list( std::vector<IBafRecord*>& records )
 	{
 		std::sort( records.begin(), records.end(), recordsort );
-		std::vector<IBafRecord*>::iterator itr = std::unique( records.begin(), records.end(), recordequal );
-		for(; itr != records.end(); itr++ )
+		//std::vector<IBafRecord*>::iterator itr = std::unique( records.begin(), records.end(), recordequal );
+
+		for(std::vector<IBafRecord*>::iterator itr = records.begin() + 1; itr != records.end(); itr++ )
 		{
-			std::vector<IBafRecord*>::iterator itr2;
-			for( itr2 = records.begin(); itr2 != itr; itr2++ )
-				if( (*itr2)->getCRC() == (*itr)->getCRC() ) break;
-			if( itr2 == itr )
+			if( (*itr)->getCRC() == (*(itr - 1))->getCRC() )
 			{
-				LOG_ERROR( "Could not find collision record for " << (*itr)->getType() << "\t" << (*itr)->getSize() << "\t" << (*itr)->getFilePosition() << "\t" << (*itr)->getCRC() );
-			}
-			else
-			{
-				LOG_INFO( "* " << std::setw(30) << (*itr)->getType() << "\t" << (*itr)->getSize() << "\t" << (*itr)->getFilePosition() << "\t" << (*itr)->getCRC() << " collided with " << std::setw(30) << (*itr2)->getType() << "\t" << (*itr2)->getSize() << "\t" << (*itr2)->getFilePosition() << "\t" << (*itr2)->getCRC() );
+				LOG_INFO( "* " << std::setw(30) << (*itr)->getType() << "\t" << (*itr)->getSize() << "\t" << (*itr)->getFilePosition() << "\t" << (*itr)->getCRC() << " collided with " << std::setw(30) << (*(itr - 1))->getType() << "\t" << (*(itr - 1))->getSize() << "\t" << (*(itr - 1))->getFilePosition() << "\t" << (*(itr - 1))->getCRC() );
 			}
 		}
 	}
