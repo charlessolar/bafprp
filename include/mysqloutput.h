245a09baa4d9f42a2fcb7c78f395a33b60ac12ee
redcomet87 <redcomet87@3e54fc21-7451-0410-8dcd-cfc14eb3ad3a>
2008-08-17 19:42:11 +0000

    mysql testing

---
+++ include/mysqloutput.h
@@ -0,0 +1,69 @@
+/*
+Copyright (C) 2008 by Charles Solar
+charlessolar@gmail.com
+
+This file is part of bafprp.
+
+bafprp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+bafprp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with bafprp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BAFPRPMYSQL_H
+#define BAFPRPMYSQL_H
+
+#include <sqltypes.h>
+
+#include "output.h"
+
+namespace bafprp
+{
+	class MYSQL : public Output
+	{
+	public:
+		// Register the output type
+		MYSQL() : Output( "mysql" ) {}
+		~MYSQL() { disconnect(); }
+
+		void record( const IBafRecord* record );
+		void error( const IBafRecord* record, const std::string& error );
+		void log( LOG_LEVEL level, const std::string& log );
+
+	private:
+		void checkDB( property_map& props );
+		void connect();
+		void disconnect();
+		std::string sanitize( const std::string& string );
+		void extractError( char* fn, SQLHANDLE handle, SQLSMALLINT type );
+
+		// property variables
+		std::string _database;
+		std::string _server;
+		std::string _user;
+		std::string _password;
+
+		std::string _table;
+
+		std::map<std::string,std::string> _columnCache;
+		time_t _cacheAge;
+
+		// private non-property variables
+		SQLHENV _env;
+		SQLHDBC _dbc;
+		bool _dbConnected;
+		
+		// This variable simply initializes a class that registers with the main output code
+		static const MYSQL registerThis;
+	};
+}
+
+#endif
